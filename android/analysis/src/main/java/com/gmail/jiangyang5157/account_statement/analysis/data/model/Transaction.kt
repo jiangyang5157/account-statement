3fa3f323897e14c5eddb58eb9d3cdd0384317af6
Yang Jiang <yang.jiang@fiserv.com>
2020-06-12 09:54:03 +1200

    analysis-cvo

--- android/analysis/src/main/java/com/gmail/jiangyang5157/account_statement/analysis/data/model/Transaction.kt
+++ android/analysis/src/main/java/com/gmail/jiangyang5157/account_statement/analysis/data/model/Transaction.kt
@@ -1,6 +1,5 @@
 package com.gmail.jiangyang5157.account_statement.analysis.data.model
 
-import com.gmail.jiangyang5157.account_statement.analysis.data.model.Field
 import com.gmail.jiangyang5157.kotlin_kit.data.model.finance.Money
 import java.util.*
 
