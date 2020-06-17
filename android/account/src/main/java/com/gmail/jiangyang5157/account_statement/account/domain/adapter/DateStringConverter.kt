97c2d1e13523f0008fb130b143f209e3efa53183
Yang Jiang <yang.jiang@fiserv.com>
2020-06-17 16:26:18 +1200

    na

--- android/account/src/main/java/com/gmail/jiangyang5157/account_statement/account/domain/adapter/DateStringConverter.kt
+++ android/account/src/main/java/com/gmail/jiangyang5157/account_statement/account/domain/adapter/DateStringConverter.kt
@@ -1,7 +1,6 @@
 package com.gmail.jiangyang5157.account_statement.account.domain.adapter
 
 import androidx.room.TypeConverter
-import com.gmail.jiangyang5157.kotlin_kit.data.model.Mapper
 import com.gmail.jiangyang5157.kotlin_kit.utils.RegexUtils
 import java.text.ParseException
 import java.text.SimpleDateFormat
